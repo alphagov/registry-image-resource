6a76752ac955ffceb63355da1b3a8b8ee94ee3d7
fredericfran-gds <43747728+fredericfran-gds@users.noreply.github.com>
2021-07-08 16:59:12 +0100

    Revert "support EC2 AWS credentials and IAM roles chain"

--- commands/in.go
+++ commands/in.go
@@ -65,8 +65,7 @@ func (i *In) Execute() error {
 
 	dest := i.args[1]
 
-	if ((req.Source.AwsAccessKeyId != "" && req.Source.AwsSecretAccessKey != "") ||
-	    (req.Source.AwsEC2Credentials)) && req.Source.AwsRegion != "" {
+	if req.Source.AwsAccessKeyId != "" && req.Source.AwsSecretAccessKey != "" && req.Source.AwsRegion != "" {
 		if !req.Source.AuthenticateToECR() {
 			return fmt.Errorf("cannot authenticate with ECR")
 		}
